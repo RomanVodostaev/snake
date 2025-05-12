ced785dad5e4074a4cff9871c5d581751b929f64
Maksym Stetsenko <mxm.stecenko@gmail.com>
2025-05-12 13:35:49 +0300

    Refactoring

--- game_field.py
+++ game_field.py
@@ -1,35 +1,6 @@
-
-import time
+from snake import Snake
 import keyboard
 import os
-
-class Snake:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.symbol = 'o'
-        self.body = [(x, y)]
-        self.direction = 'RIGHT'
-
-    def get_coords(self):
-        return self.body
-        
-    def move(self):
-        head_x, head_y = self.body[0]
-        if self.direction == 'UP':
-            new_head = (head_x, head_y - 1)
-        elif self.direction == 'DOWN':
-            new_head = (head_x, head_y + 1)
-        elif self.direction == 'LEFT':
-            new_head = (head_x - 1, head_y)
-        elif self.direction == 'RIGHT':
-            new_head = (head_x + 1, head_y)
-        
-        self.body.insert(0, new_head)
-        self.body.pop()
-
-    def grow(self):
-        self.body.append(self.body[-1])
 
 class GameField:
     def __init__(self, width, height):
@@ -38,6 +9,8 @@ class GameField:
         self.snake = Snake(width // 2, height // 2)
     
     def render(self):
+        # TODO: during render clear the screan before renderign new characters
+        
         os.system('cls' if os.name == 'nt' else 'clear')
         for y in range(self.height):
             for x in range(self.width):
@@ -61,14 +34,3 @@ class GameField:
             self.snake.direction = 'LEFT'
         elif keyboard.is_pressed('right') and self.snake.direction != 'LEFT':
             self.snake.direction = 'RIGHT'
-
-field = GameField(20, 10)
-
-try:
-    while True:
-        field.handle_input()
-        field.render()
-        field.update()
-        time.sleep(0.2)
-except KeyboardInterrupt:
-    print("\nИгра окончена!")
