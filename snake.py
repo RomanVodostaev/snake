45522e59a593bca9744be2b06e8ce86f2ba215b0
Maksym Stetsenko <mxm.stecenko@gmail.com>
2025-05-12 13:50:12 +0300

    Update

--- snake.py
+++ snake.py
@@ -5,7 +5,7 @@ class Snake:
         self.y = y
         self.symbol = 'o'
         self.body = [(x, y)]
-        self.direction = 'right'
+        self.direction = 'Right'
 
     def get_coords(self):
         return self.body
